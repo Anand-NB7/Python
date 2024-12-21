b5d69e2627577bd73803f45db49ba7a5a425e445
Anand-NB7 <anand497026@gmail.com>
2024-12-21 22:59:36 +0530

    stringformting

---
+++ 8stringformating.py
@@ -0,0 +1,6 @@
+#if= do some code only if some condition is true
+# else do something else
+
+#think tht user is applying for a credit card depending on th e age conditions
+age=int(input("Enter your age : "))
+if age >=
