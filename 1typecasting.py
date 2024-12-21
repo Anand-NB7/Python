4ee60936a6be423032567706e4fd27d5cc4e7670
Anand-NB7 <anand497026@gmail.com>
2024-12-21 19:03:49 +0530

    type cast

---
+++ 1typecasting.py
@@ -0,0 +1,25 @@
+#typecasting=the process of converting a variable from one data type to another
+# str() , int(),float(),bool()
+
+name="anand"
+age=20
+gpa=8.5
+is_stu=True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_stu))
+
+#conversion of one data type to another data type
+gpa=int(gpa)
+print(gpa)
+
+age=float(age)
+print(age)
+
+age=str(age)
+print(age)
+
+name=bool(name)
+print(name)
