63f36834033f54b08bbe16c4d52be674c8a29fdc
Anand-NB7 <anand497026@gmail.com>
2024-12-21 21:11:04 +0530

    simple arithmatic operations

---
+++ 4arithmatic.py
@@ -0,0 +1,18 @@
+# some of the arithmatic operations
+
+friends=5
+#friends=friends+1 #or u can also type friends += 1 , augumented assignment operator
+#friends=friends-2
+#friends -=2
+#friends=friends*3
+#friends *= 3
+#friends=friends/2
+#friends /= 2
+#friends=friends**2
+#friends **= 2
+remainder=friends%5 #it is a modulus
+
+
+#print(friends)
+#print(remainder)
+
