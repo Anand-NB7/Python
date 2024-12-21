887477f2dbc1f8bec713f3de05d992a30e7f1a5b
Anand-NB7 <anand497026@gmail.com>
2024-12-21 21:23:47 +0530

    inbuilt math related function

---
+++ 5builtinfunctions.py
@@ -0,0 +1,13 @@
+# here we are seeing some ofthe inbuilt math related functions
+
+x=3.14
+y=-4
+z=5
+
+#result=round(x) #it is a function to round of it means that obsolute value
+#result=abs(y) #absolute value is a value distance away from the zero as a whole number
+#result=pow(4,3) # to find the power
+#result=max(x,y,z) #max value btween x y z
+#result=min(x,y,z)  # min value between x y z
+
+print(result)
